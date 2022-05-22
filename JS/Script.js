9bfa051c07e3cf1e2e67d554e6e04239c8f8b54d
NajibMagdi <meladmagdi9@gmail.com>
2022-05-22 07:41:52 -0700

    This First Version Of CP Mina Same

---
+++ JS/Script.js
@@ -0,0 +1,25 @@
+var btn = document.getElementById("btn-open-window");
+var MainNav = document.getElementById("main-nav-links"); 
+var MainNavLink = MainNav.querySelectorAll("ul li a");
+let Usermenubtn = document.getElementsByClassName("linkUser")[0];
+let useractive = document.getElementById("ulUser");
+
+btn.onclick = function () {
+    MainNav.classList.toggle('active');
+    if(MainNav.offsetWidth <= 70) { /*For End Of Project Find Way To make 70 Is Dainamic*/
+        setTimeout(function () {
+            MainNavLink.forEach(e => {
+                e.classList.add('disAnchor')
+            })
+        },200)
+    }else {
+        MainNavLink.forEach(e => {
+            e.classList.remove('disAnchor')
+        })
+    }
+}
+
+Usermenubtn.onclick = () => {
+    useractive.classList.toggle("activeUserMenu");
+}
+
